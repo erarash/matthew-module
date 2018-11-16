d5a9a9cbc6397c1a369ceea1b391af06c918a6d8
Arash <erani.arash@gmail.com>
2018-11-15 16:42:11 -0800

    mongo seeded

--- database/index.js
+++ database/index.js
@@ -1,23 +1,56 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+//POSTGRES////////////////////////////////////////////////////////////////////
+// const  = require("");
+// require("dotenv").config();
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-);
+// const connection = new (
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.HOSTNAME,
+//     dialect: "postgres"
+//   }
+// );
 
-connection
-  .authenticate()
-  .then(() => {
-    console.log('MySQL connection has been established successfully');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to database: ', err);
-  });
+// connection
+//   .authenticate()
+//   .then(() => {
+//     console.log("postgres connection has been established successfully");
+//   })
+//   .catch(err => {
+//     console.error("Unable to connect to database: ", err);
+//   });
 
-module.exports = connection;
+// module.exports = connection;
+////////////////////////////////////////////////////////////////////////////
+
+let mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mattrev");
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("connected to mongodb");
+});
+
+var reviewSchema = new mongoose.Schema({
+  user: String,
+  prodRating: Number,
+  yesRating: Number,
+  noRating: Number,
+  date: { type: date },
+  body: String,
+  verified: Boolean,
+  recommend: Boolean,
+  size: Number,
+  width: Number,
+  comfort: Number,
+  quality: Number,
+  response: String,
+  prodId: Number,
+  header: String
+});
+
+var review = mongoose.model("review", reviewSchema);
+
+module.exports = { review };
--- database/models.js
+++ database/models.js
@@ -1,42 +1,42 @@
-const Sequelize = require('sequelize');
-const connection = require('./');
+// const Sequelize = require('sequelize');
+// const connection = require('./');
 
-const Comments = connection.define(
-  'comment',
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    user: Sequelize.STRING,
-    prodRating: Sequelize.INTEGER,
-    yesRating: Sequelize.INTEGER,
-    noRating: Sequelize.INTEGER,
-    date: Sequelize.DATE,
-    body: Sequelize.TEXT,
-    verified: Sequelize.BOOLEAN,
-    recommend: Sequelize.BOOLEAN,
-    size: Sequelize.INTEGER,
-    width: Sequelize.INTEGER,
-    comfort: Sequelize.INTEGER,
-    quality: Sequelize.INTEGER,
-    response: Sequelize.TEXT,
-    prodId: Sequelize.INTEGER,
-    header: Sequelize.TEXT,
-  },
-  {
-    createdAt: false,
-  },
-);
+// const Comments = connection.define(
+//   'comment',
+//   {
+//     id: {
+//       type: Sequelize.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     user: Sequelize.STRING,
+//     prodRating: Sequelize.INTEGER,
+//     yesRating: Sequelize.INTEGER,
+//     noRating: Sequelize.INTEGER,
+//     date: Sequelize.DATE,
+//     body: Sequelize.TEXT,
+//     verified: Sequelize.BOOLEAN,
+//     recommend: Sequelize.BOOLEAN,
+//     size: Sequelize.INTEGER,
+//     width: Sequelize.INTEGER,
+//     comfort: Sequelize.INTEGER,
+//     quality: Sequelize.INTEGER,
+//     response: Sequelize.TEXT,
+//     prodId: Sequelize.INTEGER,
+//     header: Sequelize.TEXT,
+//   },
+//   {
+//     createdAt: false,
+//   },
+// );
 
-connection
-  .sync()
-  .then(() => {
-    console.log('MySQL connection synced');
-  })
-  .catch((err) => {
-    console.error('Synced failed: ', err);
-  });
+// connection
+//   .sync()
+//   .then(() => {
+//     console.log('MySQL connection synced');
+//   })
+//   .catch((err) => {
+//     console.error('Synced failed: ', err);
+//   });
 
-module.exports = Comments;
+// module.exports = Comments;
