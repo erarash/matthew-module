cb253617a13d4f9cb4fbd4020d6dd0ddafbdc8d2
Arash <erani.arash@gmail.com>
2018-11-15 16:23:17 -0800

    mongo seeding complete

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
-const Sequelize = require('sequelize');
-const Comments = require('../database/models');
+const mongoose = require("mongoose");
+const Comments = require("../database/index.js");
 
 const { Op } = Sequelize;
 
@@ -7,10 +7,10 @@ module.exports = {
   get: (req, res) => {
     const { id } = req.params;
     Comments.findAll({ where: { prodId: id } })
-      .then((result) => {
+      .then(result => {
         res.status(200).send(result);
       })
-      .catch((err) => {
+      .catch(err => {
         console.error(err);
       });
   },
@@ -20,39 +20,39 @@ module.exports = {
     const { filters } = req.query;
     const { id } = req.params;
     let field;
-    if (type === 'relevant') {
-      field = 'user';
-    } else if (type === 'helpfulButton') {
-      field = 'yesRating';
-    } else if (type === 'newest') {
-      field = 'date';
+    if (type === "relevant") {
+      field = "user";
+    } else if (type === "helpfulButton") {
+      field = "yesRating";
+    } else if (type === "newest") {
+      field = "date";
     }
 
-    if (filters !== '[]') {
+    if (filters !== "[]") {
       console.log(filters);
       Comments.findAll({
-        order: [[`${field}`, 'DESC']],
+        order: [[`${field}`, "DESC"]],
         limit: parseInt(limit, 10),
-        where: { prodRating: { [Op.or]: JSON.parse(filters) }, prodId: id },
+        where: { prodRating: { [Op.or]: JSON.parse(filters) }, prodId: id }
       })
-        .then((result) => {
+        .then(result => {
           res.status(200).send(result);
         })
-        .catch((err) => {
+        .catch(err => {
           console.error(err);
         });
     } else {
       Comments.findAll({
-        order: [[`${field}`, 'DESC']],
+        order: [[`${field}`, "DESC"]],
         limit: parseInt(limit, 10),
-        where: { prodId: id },
+        where: { prodId: id }
       })
-        .then((result) => {
+        .then(result => {
           res.status(200).send(result);
         })
-        .catch((err) => {
+        .catch(err => {
           console.error(err);
         });
     }
-  },
+  }
 };
